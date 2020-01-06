e94c2c321efbe9bfa8b0b0900950e9d56f2cba0e
PatriciaLocatelli <patriciawalau@hotmail.com>
2020-01-06 10:23:25 -0300

    Exercícios Lógica 06/01/2020

--- src/logica/estruturacondicional/Exercicio8.java
+++ src/logica/estruturacondicional/Exercicio8.java
@@ -1,10 +1,53 @@
+/*
+Em um país imaginário denominado Lisarb, todos os habitantes ficam felizes em pagar seus impostos, pois sabem
+que nele não existem políticos corruptos e os recursos arrecadados são utilizados em benefício da população, sem
+qualquer desvio. A moeda deste país é o Rombus, cujo símbolo é o R$.
+Leia um valor com duas casas decimais, equivalente ao salário de uma pessoa de Lisarb. Em seguida, calcule e
+mostre o valor que esta pessoa deve pagar de Imposto de Renda, segundo a tabela abaixo.
+Lembre que, se o salário for R$ 3002.00, a taxa que incide é de 8% apenas sobre R$ 1000.00, pois a faixa de
+salário que fica de R$ 0.00 até R$ 2000.00 é isenta de Imposto de Renda. No exemplo fornecido (abaixo), a taxa é
+de 8% sobre R$ 1000.00 + 18% sobre R$ 2.00, o que resulta em R$ 80.36 no total. O valor deve ser impresso com
+duas casas decimais.
+ */
 package logica.estruturacondicional;
+
+import java.util.Locale;
+import java.util.Scanner;
 
 public class Exercicio8 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
+		double salario = sc.nextDouble();
+		double imposto = calculadoraDeImposto(salario);
 
+		if (imposto == 0.0) {
+			System.out.println("Isento");
+		} else {
+			System.out.printf("Imposto %.2f", imposto);
+		}
+
+		sc.close();
+
+	}
+
+	public static double calculadoraDeImposto(double salario) {
+		double imposto;
+		if (salario <= 2000.00) {
+			return 0.0;
+		} else if (salario > 2000.00 && salario <= 3000.00) {
+			return ((salario - 2000) * 0.08);
+
+		} else if (salario > 3000.00 && salario <= 4500.00) {// 3000-2000 = 1000 * 0.08
+			imposto = 1000 * 0.08;
+			return ((salario - 3000) * 0.18) + imposto;
+
+		} else { // 4500-2000 = 2500 // 1500 * 0.18 // 1000 * 0.08
+			imposto = (1500 * 0.18) + (1000 * 0.08);
+			return ((salario - 4500) * 0.28 + imposto);
+
+		}
 	}
 
 }
---
+++ src/logica/estruturarepetitiva/topicowhile/Execicio1.java
@@ -0,0 +1,30 @@
+/*
+ * 
+Escreva um programa que repita a leitura de uma senha até que ela seja válida. Para cada leitura de senha
+incorreta informada, escrever a mensagem "Senha Invalida". Quando a senha for informada corretamente deve ser
+impressa a mensagem "Acesso Permitido" e o algoritmo encerrado. Considere que a senha correta é o valor 2002.
+ */
+package logica.estruturarepetitiva.topicowhile;
+
+import java.util.Scanner;
+
+public class Execicio1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int senha;
+		
+		while (true) {
+			senha = sc.nextInt();
+			if (senha != 2002) {
+				System.out.println("Senha Inválida");
+			} else {
+				System.out.println("Acesso permitido");
+				break;
+			}
+		}
+
+		sc.close();
+	}
+
+}
