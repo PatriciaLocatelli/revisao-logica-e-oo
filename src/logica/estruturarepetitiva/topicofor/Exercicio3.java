abce500c430b6f4238be1c08f45863dd2bfb274f
PatriciaLocatelli <patriciawalau@hotmail.com>
2020-01-07 09:52:11 -0300

    Exercícios de lógica 07/01/2020

---
+++ src/logica/estruturarepetitiva/topicofor/Exercicio3.java
@@ -0,0 +1,26 @@
+/*
+ * 
+ Leia 1 valor inteiro N, que representa o número de casos de teste que vem a seguir. Cada caso de teste consiste
+de 3 valores reais, cada um deles com uma casa decimal. Apresente a média ponderada para cada um destes
+conjuntos de 3 valores, sendo que o primeiro valor tem peso 2, o segundo valor tem peso 3 e o terceiro valor tem
+peso 5.
+ */
+package logica.estruturarepetitiva.topicofor;
+
+import java.util.Scanner;
+
+public class Exercicio3 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int n = sc.nextInt();
+		double mediaPonderada = 0;
+		for (int i = 1; i <=n; i++) {
+			double x = sc.nextDouble();
+		}
+		
+		sc.close();
+	}
+
+}
--- src/logica/estruturarepetitiva/topicowhile/Exercicio2.java
+++ src/logica/estruturarepetitiva/topicowhile/Exercicio2.java
@@ -6,10 +6,38 @@ menos uma de duas coordenadas for NULA (nesta situação sem escrever mensagem a
  */
 package logica.estruturarepetitiva.topicowhile;
 
+import java.util.Scanner;
+
 public class Exercicio2 {
 
 	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
 
+		while (true) {
+			int x = sc.nextInt();
+			int y = sc.nextInt();
+			String quadrante;
+			if (x == 0 || y == 0) {
+				break;
+			} else {
+				quadrante = qualQuadrante(x, y);
+				System.out.println(quadrante);
+			}
+		}
+
+		sc.close();
+	}
+
+	public static String qualQuadrante(int x, int y) {
+		if (x > 0 && y > 0) {
+			return "Primeiro Quadrante";
+		} else if (x < 0 && y > 0) {
+			return "Segundo Quadrante";
+		} else if (x < 0 && y < 0) {
+			return "Terceiro Quadrante";
+		} else {
+			return "Quarto Quadrante";
+		}
 	}
 
 }
